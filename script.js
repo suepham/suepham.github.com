771192c85590b2e68179ace15693542e2b8ebb7d
Sue Pham <suepham@ucdavis.edu>
2020-03-24 13:08:33 -0500

    updated portfolio

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const arrow = document.getElementById("down");
 
-function smoothScroll(event) {
-    event.preventDefault();
-
-    const targetID = event.target.getAttribute("href");
-    const targetAnchor = document.querySelector(targetID);
-
-    const originalTop = Math.floor(targetAnchor.getBoundingClientRect().top);
-    window.scrollBy({top: originalTop, left: 0, behavior: "smooth"});
-
-};
-
 arrow.addEventListener("click", function(){
     const work = document.getElementById("featureOne");
 
@@ -18,4 +7,58 @@ arrow.addEventListener("click", function(){
 	
 	window.scrollBy({top: feature, left: 0, behavior: "smooth"});
 
+});
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
+
+const navi = document.querySelectorAll("nav ul li a");
+const topMe = document.getElementById("welcome");
+const aboutMe = document.getElementById("about");
+const features = document.getElementsByClassName("feature");
+const works = document.getElementById("work");
+
+navi[0].addEventListener("click", function(e){
+  e.preventDefault();
+
+  topMe.style.display = "block";
+  aboutMe.style.display = "none";
+  for (i=0; i<4; i++) {
+    features[i].style.display = "block";
+  }
+  work.style.display = "none";
+});
+
+navi[1].addEventListener("click", function(e){
+  e.preventDefault();
+
+  topMe.style.display = "none";
+  for (i=0; i<4; i++) {
+    features[i].style.display = "none";
+  }
+  work.style.display = "block";
+  aboutMe.style.display = "none";
+});
+
+navi[2].addEventListener("click", function(e){
+  e.preventDefault();
+
+  topMe.style.display = "none";
+  for (i=0; i<4; i++) {
+    features[i].style.display = "none";
+  }
+  work.style.display = "none";
+  aboutMe.style.display = "block";
 });
